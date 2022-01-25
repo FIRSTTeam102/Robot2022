1bbd410c014914c758db0522ce260a289e5f3903
Sean <robosean00@gmail.com>
2022-01-25 18:24:19 -0500

    Added turndegrees command based on gyro

---
+++ src/main/cpp/commands/SwerveDrive/TurnDegreesGyro.cpp
@@ -0,0 +1,30 @@
+#include "commands/SwerveDrive/TurnDegreesGyro.h"
+
+TurnDegreesGyro::TurnDegreesGyro(SwerveDrive* pSwerveDrive, double speed, double degrees): mpSwerveDrive{pSwerveDrive}, mSpeed{speed}, mTargetDegs{degrees} {
+	// Use addRequirements() here to declare subsystem dependencies.
+	AddRequirements(pSwerveDrive);
+}
+
+// Called when the command is initially scheduled.
+void TurnDegreesGyro::Initialize() {
+	mpSwerveDrive->resetGyro();
+}
+
+// Called repeatedly when this Command is scheduled to run
+void TurnDegreesGyro::Execute() {
+	mpSwerveDrive->vectorSwerve(0, 0, ( (mTargetDegs >= 0) ? mSpeed : -mSpeed ));
+}
+
+// Called once the command ends or is interrupted.
+void TurnDegreesGyro::End(bool interrupted) {
+	mpSwerveDrive->stopDrive();
+}
+
+// Returns true when the command should end.
+bool TurnDegreesGyro::IsFinished() {
+	if (mTargetDegs >= 0) {
+		return (mpSwerveDrive->getGyroAngle() >= mTargetDegs);
+	} else {
+		return (mpSwerveDrive->getGyroAngle() <= mTargetDegs);
+	}
+}
