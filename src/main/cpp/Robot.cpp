457c2d959bd2ff99a339023321acb9aff61fecac
Sean <robosean00@gmail.com>
2022-02-01 18:59:28 -0500

    add compressor

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,7 +3,9 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
 
-void Robot::RobotInit() {}
+void Robot::RobotInit() {
+	mCompressor.Start();
+}
 
 /**
  * This function is called every robot packet, no matter the mode. Use
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -2,6 +2,7 @@
 
 #include <frc/TimedRobot.h>
 #include <frc2/command/Command.h>
+#include <frc/Compressor.h>
 
 #include "RobotContainer.h"
 #include "subsystems/Lights.h"
@@ -21,6 +22,8 @@ class Robot : public frc::TimedRobot {
 	private:
 		// Have it null by default so that if testing teleop it
 		// doesn't have undefined behavior and potentially crash.
+		frc::Compressor mCompressor{frc::PneumaticsModuleType::REVPH};
+
 		frc2::Command* mAutonomousCommand = nullptr;
 
 		RobotContainer* mContainer = RobotContainer::GetInstance();
