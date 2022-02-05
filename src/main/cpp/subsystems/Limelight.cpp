98a072b55c6c5cacde0796547c6675c717f059a1
Seth <swcaskey@gmail.com>
2022-02-05 10:14:20 -0500

    Shoot from anywhere Test

--- src/main/cpp/subsystems/Limelight.cpp
+++ src/main/cpp/subsystems/Limelight.cpp
@@ -22,8 +22,64 @@ double Limelight::calculateShootDistance() {
 }
 
 double Limelight::getShootSpeed() {
-	if (d >= 195.0) {
+	if (d >= 330.0) {
+		rpm = 3220;
+	} else if (d >= 330.0) {
+		rpm = 3205;
+	} else if (d >= 325.0) {
+		rpm = 3198;
+	} else if (d >= 320.0) {
+		rpm = 3191;
+	} else if (d >= 315.0) {
+		rpm = 3184;
+	} else if (d >= 310.0) {
+		rpm = 3177;
+	} else if (d >= 305.0) {
+		rpm = 3170;
+	} else if (d >= 300.0) {
+		rpm = 3163;
+	} else if (d >= 295.0) {
+		rpm = 3157;
+	} else if (d >= 290.0) {
+		rpm = 3150;
+	} else if (d >= 285.0) {
+		rpm = 3144;
+	} else if (d >= 280.0) {
+		rpm = 3137;
+	} else if (d >= 275.0) {
+		rpm = 3131;
+	} else if (d >= 270.0) {
+		rpm = 3125;
+	} else if (d >= 265.0) {
+		rpm = 3119;
+	} else if (d >= 260.0) {
+		rpm = 3113;
+	} else if (d >= 255.0) {
+		rpm = 3107;
+	} else if (d >= 250.0) {
+		rpm = 3101;
+	} else if (d >= 245.0) {
+		rpm = 3096;
+	} else if (d >= 240.0) {
+		rpm = 3090;
+	} else if (d >= 235.0) {
+		rpm = 3085;
+	} else if (d >= 230.0) {
+		rpm = 3080;
+	} else if (d >= 225.0) {
+		rpm = 3074;
+	} else if (d >= 220.0) {
+		rpm = 3069;
+	} else if (d >= 215.0) {
+		rpm = 3064;
+	} else if (d >= 210.0) {
+		rpm = 3059;
+	} else if (d >= 205.0) {
+		rpm = 3055;
+	} else if (d >= 200.0) {
 		rpm = 3050;
+	} else if (d >= 195.0) {
+		rpm = 3045;
 	} else if (d >= 190.0) {
 		rpm = 3041;
 	} else if (d >= 185.0) {
@@ -74,8 +130,14 @@ double Limelight::getShootSpeed() {
 		rpm = 2969;
 	} else if (d >= 70.0) {
 		rpm = 2968;
-	} else {
+	} else if (d >= 65.0) {
+		rpm = 2966;
+	} else if (d >= 60.0) {
 		rpm = 2965;
+	} else if (d >= 55.0) {
+		rpm = 2963;
+	} else {
+		rpm = 2961;
 	}
 
 	// return rpm / ShooterConstants::kMaxRpm; // convert to percent output
@@ -83,8 +145,62 @@ double Limelight::getShootSpeed() {
 }
 
 double Limelight::getServoAngle() {
-	if (d >= 195.0) {
+	if (d >= 330.0) {
+		hoodAngle = 66.65;
+	} else if (d >= 325.0) {
+		hoodAngle = 67.58;
+	} else if (d >= 320.0) {
+		hoodAngle = 67.89;
+	} else if (d >= 315.0) {
+		hoodAngle = 68.20;
+	} else if (d >= 310.0) {
+		hoodAngle = 68.52;
+	} else if (d >= 305.0) {
+		hoodAngle = 68.83;
+	} else if (d >= 300.0) {
+		hoodAngle = 69.15;
+	} else if (d >= 295.0) {
+		hoodAngle = 69.47;
+	} else if (d >= 290.0) {
+		hoodAngle = 69.79;
+	} else if (d >= 285.0) {
+		hoodAngle = 70.11;
+	} else if (d >= 280.0) {
+		hoodAngle = 70.43;
+	} else if (d >= 275.0) {
+		hoodAngle = 70.76;
+	} else if (d >= 270.0) {
+		hoodAngle = 71.08;
+	} else if (d >= 265.0) {
+		hoodAngle = 71.41;
+	} else if (d >= 260.0) {
+		hoodAngle = 71.73;
+	} else if (d >= 255.0) {
+		hoodAngle = 72.06;
+	} else if (d >= 250.0) {
+		hoodAngle = 72.39;
+	} else if (d >= 245.0) {
+		hoodAngle = 72.72;
+	} else if (d >= 240.0) {
+		hoodAngle = 73.05;
+	} else if (d >= 235.0) {
+		hoodAngle = 73.39;
+	} else if (d >= 230.0) {
+		hoodAngle = 73.72;
+	} else if (d >= 225.0) {
+		hoodAngle = 74.06;
+	} else if (d >= 220.0) {
+		hoodAngle = 74.40;
+	} else if (d >= 215.0) {
+		hoodAngle = 74.73;
+	} else if (d >= 210.0) {
+		hoodAngle = 75.07;
+	} else if (d >= 205.0) {
+		hoodAngle = 75.41;
+	} else if (d >= 200.0) {
 		hoodAngle = 75.75;
+	} else if (d >= 195.0) {
+		hoodAngle = 76.10;
 	} else if (d >= 190.0) {
 		hoodAngle = 76.44;
 	} else if (d >= 185.0) {
@@ -133,6 +249,8 @@ double Limelight::getServoAngle() {
 		hoodAngle = 84.20;
 	} else if (d >= 75.0) {
 		hoodAngle = 84.56;
+	} else if (d >= 70.0) {
+		hoodAngle = 84.92;
 	} else if (d >= 70.0) {
 		hoodAngle = 84.92;
 	} else {
