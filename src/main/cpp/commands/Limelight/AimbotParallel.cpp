78860eab953c4375d6ba14b102c2454b0fed8cae
Seth <swcaskey@gmail.com>
2022-02-09 18:38:28 -0500

    First real test

--- src/main/cpp/commands/Limelight/AimbotParallel.cpp
+++ src/main/cpp/commands/Limelight/AimbotParallel.cpp
@@ -2,5 +2,5 @@
 
 AimbotParallel::AimbotParallel(Limelight *pLimelight, Shooter *pShooter, SwerveDrive *pSwerveDrive) : mpLimelight{pLimelight} {
 	SetName("AimbotParallel");
-	AddCommands(StartShooter(pShooter, mpLimelight->getShootSpeed(), true), YawToTarget(pLimelight, pSwerveDrive));
+	AddCommands(/* StartShooter(pShooter, mpLimelight->getShootSpeed(), true), */ YawToTarget(pLimelight, pSwerveDrive));
 }
