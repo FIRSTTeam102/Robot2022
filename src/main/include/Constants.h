4b783e36fc6d9459795c85335954340e657642e4
Seth <swcaskey@gmail.com>
2022-02-09 20:14:52 -0500

    Limelight Official Offical Very OFFICAL testing for tomorrow because I want to test but the build ended

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -44,14 +44,14 @@ namespace ShooterConstants {
 
 namespace SwerveDriveConstants {
 	// Motor ports
-	const int kFLDrive = 1;
-	const int kFLTurn = 2;
-	const int kFRDrive = 3;
-	const int kFRTurn = 4;
-	const int kBRDrive = 5;
-	const int kBRTurn = 6;
-	const int kBLDrive = 7;
-	const int kBLTurn = 8;
+	const int kFLDrive = 21;
+	const int kFLTurn = 22;
+	const int kFRDrive = 23;
+	const int kFRTurn = 24;
+	const int kBRDrive = 25;
+	const int kBRTurn = 26;
+	const int kBLDrive = 27;
+	const int kBLTurn = 28;
 
 	// Encoder Ports
 	const int kFLEnc = 0;
@@ -60,10 +60,10 @@ namespace SwerveDriveConstants {
 	const int kBLEnc = 3;
 
 	// Calibration Values
-	const int kFLOffset = 573;
-	const int kFROffset = 412;
-	const int kBROffset = 2686;
-	const int kBLOffset = 1176;
+	const int kFLOffset = 603;
+	const int kFROffset = 415;
+	const int kBROffset = 2672;
+	const int kBLOffset = 1156;
 
 	const double kMaxSpeed = 1.00;
 }
