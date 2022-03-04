0f046078b8ca729e2c1e2f492eebae53514539f8
gearheads102 <firstteam102@gmail.com>
2022-03-04 18:42:15 -0500

    minor auto fixes

--- src/main/cpp/commands/Autonomous/MainAutonomous.cpp
+++ src/main/cpp/commands/Autonomous/MainAutonomous.cpp
@@ -3,21 +3,23 @@
 MainAutonomous::MainAutonomous(Indexer* pIndexer, Intake* pIntake, Limelight* pLM, Shooter* pShooter, SwerveDrive* pSwerveDrive) : mpIndexer{pIndexer}, mpIntake{pIntake}, mpLM{pLM}, mpShooter{pShooter}, mpSwerve{pSwerveDrive} {
 	AddCommands(
 		SetArm(mpIntake, frc::DoubleSolenoid::Value::kForward),
-		SetRollers(mpIntake, MotorDirection::kForward),
 		frc2::WaitCommand(0.6_s),
+		SetRollers(mpIntake, MotorDirection::kForward),
 		SetAnglesCommand(mpSwerve, 0.0),
-		frc2::WaitCommand(0.5_s),
-		SetSpeedsTimed(mpSwerve, 0.65, 1.3),
+		frc2::WaitCommand(0.3_s),
+		SetSpeedsTimed(mpSwerve, 0.65, 1.5),
 		frc2::WaitCommand(0.2_s),
-		TurnDegreesGyro(mpSwerve, 0.75, 170),
+		SetRollers(mpIntake, MotorDirection::kOff),
+		TurnDegreesGyro(mpSwerve, 0.85, 180),
 		// LM stuff when ready
 		// SetArm(mpIntake, frc::DoubleSolenoid::Value::kReverse),
 		StartShooter(mpShooter, ShooterConstants::kMedSpeed),
+		frc2::WaitCommand(0.2_s),
 		SetIndexer(mpIndexer, MotorDirection::kForward),
+		SetRollers(mpIntake, MotorDirection::kForward),
 		frc2::WaitUntilCommand([&] { return mpIndexer->getSwitch(); }),
 		frc2::WaitCommand(2.0_s),
 		SetIndexer(mpIndexer, MotorDirection::kOff),
-		StopShooter(mpShooter),
-		SetRollers(mpIntake, MotorDirection::kOff)
+		StopShooter(mpShooter)
 	);
 }
